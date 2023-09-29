fb6960cd922d518da82271719649ee1a44f49f05
John <ashton.basile@gmail.com>
2023-09-29 16:43:33 +0100

    I wrote the first two regular tests of the calculator

--- cypress/integration/calculatorTests/calculator.spec.js
+++ cypress/integration/calculatorTests/calculator.spec.js
@@ -1,3 +1,26 @@
+/*
+You should write tests to ensure the following 
+requirements are met:
+
+1. Do the number buttons update the display of the running total?
+
+2. Do the arithmetical operations update the display with the 
+result of the operation?
+
+3. Can multiple operations be chained together?
+
+4. Is the output as expected for a range of numbers 
+(for example, positive, negative, decimals and 
+  very large numbers)?
+
+5.   What does the code do in exceptional circumstances? 
+Specifically, if you divide by zero, what is the effect? 
+Write a test to describe what you'd prefer to happen, 
+and then correct the code to make that test pass 
+(you will need to modify the Calculator model to 
+  meet this requirement).
+*/
+
 describe("Calculator", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import Calculator from '../containers/Calculator';
-import {render, fireEvent} from '@testing-library/react';
+import React from "react";
+import Calculator from "../containers/Calculator";
+import { render, fireEvent } from "@testing-library/react";
 
-describe('Calculator', () => {
+/*
+calculator.add() - add 1 to 4 and get 5
+calculator.subtract() subtract 4 from 7 and get 3
+calculator.multiply() - multiply 3 by 5 and get 15
+calculator.divide() - divide 21 by 7 and get 3
+calculator.numberClick() - concatenate multiple number button clicks
+calculator.operatorClick() - chain multiple operations together
+calculator.clearClick() - clear the running total without affecting the calculation
+*/
+
+describe("Calculator", () => {
   let container;
   beforeEach(() => {
-    container = render(<Calculator/>)
-  })
+    container = render(<Calculator />);
+  });
 
-  it('should change running total on number enter', () => {
-    const button4 = container.getByTestId('number4');
-    const runningTotal = container.getByTestId('running-total');
+  it("should change running total on number enter", () => {
+    const button4 = container.getByTestId("number4");
+    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button4);
-    expect(runningTotal.textContent).toEqual('4');
-  })
-})
+    expect(runningTotal.textContent).toEqual("4");
+  });
 
+  it("should add 1 to 4 and get 5", () => {
+    const runningTotal = container.getByTestId("running-total");
+    const button1 = container.getByTestId("number1");
+    fireEvent.click(button1);
+    const button2 = container.getByTestId("operator-add");
+    fireEvent.click(button2);
+    const button3 = container.getByTestId("number4");
+    fireEvent.click(button3);
+    const button4 = container.getByTestId("operator-equals");
+    fireEvent.click(button4);
+    expect(runningTotal.textContent).toEqual("5");
+  });
+
+  it("should subtract 4 from 7 and get 3", () => {
+    const runningTotal = container.getByTestId("running-total");
+    const button1 = container.getByTestId("number7");
+    fireEvent.click(button1);
+    const button2 = container.getByTestId("operator-subtract");
+    fireEvent.click(button2);
+    const button3 = container.getByTestId("number4");
+    fireEvent.click(button3);
+    const button4 = container.getByTestId("operator-equals");
+    fireEvent.click(button4);
+    expect(runningTotal.textContent).toEqual("3");
+  });
+});
